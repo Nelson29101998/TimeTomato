208d861581c40d634b8c7bfbcdb9b72abf057c3a
TinCode1998 <nelsonmouatvergara@gmail.com>
2025-07-10 12:28:48 -0400

    Añadir comentario para aclarar la función del botón de cálculo de velocidad de lectura

--- TimeTomato/TimeTomato.py
+++ TimeTomato/TimeTomato.py
@@ -39,6 +39,7 @@ def index() -> rx.Component:
                 ),
             ),
             
+            # Para calcular la velocidad de lectura
             rx.button(
                 "Calcular de PM",
                 class_name="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800",
