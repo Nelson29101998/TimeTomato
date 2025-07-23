e48ac53515005d418e469000cd0b58b2960ed0de
TinCode1998 <nelsonmouatvergara@gmail.com>
2025-07-23 12:40:22 -0400

    Refactor la función index para mejorar la estructura y el diseño de la interfaz de usuario

--- TimeTomato/TimeTomato.py
+++ TimeTomato/TimeTomato.py
@@ -36,91 +36,97 @@ class NumeroVelocidad(rx.State):
 def index() -> rx.Component:
     return rx.box(
         rx.container(
-            rx.vstack(
+            rx.container(
                 rx.vstack(
-                    rx.center(
-                        rx.image(src="/images/logo.png",
-                                 alt="logo de timetomato", height="auto"),
-                        class_name="bg-linear-to-r from-cyan-500 to-blue-400",
-                        border_radius="15px",
-                        border_width="medium",
-                        width="50%",
+                    rx.vstack(
+                        rx.center(
+                            rx.image(src="/images/logo.png",
+                                     alt="logo de timetomato", height="auto"),
+                            class_name="bg-linear-to-r from-cyan-500 to-blue-400",
+                            border_radius="15px",
+                            border_width="medium",
+                            width="300",
+                        ),
+                        rx.center(
+                            rx.image(src="/images/tomateTiempo.jpeg",
+                                     alt="logo de timetomato", height="auto"),
+                            class_name="shadow-xl/20",
+                            border_width="medium",
+                            width="250",
+                        ),
+                        align="center",
                     ),
-                    rx.center(
-                        rx.image(src="/images/tomateTiempo.jpeg",
-                                 alt="logo de timetomato", height="auto"),
-                        class_name="shadow-xl/20",
-                        border_width="medium",
-                        width="30%",
+                    spacing="4",
+                ),
+            ),
+
+            rx.container(
+                rx.vstack(
+                    # Número de palabras
+                    rx.vstack(
+                        rx.heading("Número de palabra de texto:"),
+                        rx.input(
+                            type="number",
+                            placeholder="Ingresa los números...",
+                            border_radius="10px",
+                            value=NumeroVelocidad.numeroPalabra,
+                            on_change=NumeroVelocidad.set_numeroPalabra,
+                        ),
+                        class_name="fas fa-file-alt",
                     ),
+
+                    # Tiempo empleado
+                    rx.vstack(
+                        rx.heading("Tiempo empleado en segundos:"),
+                        rx.input(
+                            type="number",
+                            placeholder="Ingresa los tiempo...",
+                            border_radius="10px",
+                            value=NumeroVelocidad.tiempoEmpleado,
+                            on_change=NumeroVelocidad.set_tiempoEmpleado,
+                        ),
+                        class_name="fas fa-file-alt",
+                    ),
+                    spacing="4",
+                ),
+            ),
+
+            rx.container(
+                rx.vstack(
+                    rx.hstack(
+                        # Para calcular la velocidad de lectura
+                        rx.button(
+                            "Calcular de PM",
+                            class_name="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800",
+                            on_click=NumeroVelocidad.calcTomato,
+                        ),
+                        rx.button(
+                            "Limpiar",
+                            class_name="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800",
+                            on_click=NumeroVelocidad.limpiar,
+                        ),
+                    ),
+
+                    rx.hstack(
+                        rx.cond(
+                            NumeroVelocidad.numVisible,
+                            rx.text(
+                                "Total de PM es: ", NumeroVelocidad.totalTomato, " palabras por minuto",
+                                size="5",
+                            ),
+                            rx.divider(),
+                        ),
+                    ),
+
+                    spacing="2",
                     align="center",
                 ),
-                spacing="4",
             ),
+            class_name="container mx-auto max-w-sm rounded-lg border-solid border-4 border-indigo-600 bg-sky-300 p-5",
+            bg="var(--primary-color)",
+            padding="50px",
         ),
-
-        rx.container(
-            rx.vstack(
-                # Número de palabras
-                rx.hstack(
-                    rx.heading("Número de palabra de texto:"),
-                    rx.input(
-                        type="number",
-                        placeholder="Ingresa los números...",
-                        border_radius="10px",
-                        value=NumeroVelocidad.numeroPalabra,
-                        on_change=NumeroVelocidad.set_numeroPalabra,
-                    ),
-                ),
-
-                # Tiempo empleado
-                rx.hstack(
-                    rx.heading("Tiempo empleado en segundos:"),
-                    rx.input(
-                        type="number",
-                        placeholder="Ingresa los tiempo...",
-                        border_radius="10px",
-                        value=NumeroVelocidad.tiempoEmpleado,
-                        on_change=NumeroVelocidad.set_tiempoEmpleado,
-                    ),
-                ),
-                spacing="4",
-            ),
-        ),
-
-        rx.container(
-            rx.vstack(
-                rx.hstack(
-                    # Para calcular la velocidad de lectura
-                    rx.button(
-                        "Calcular de PM",
-                        class_name="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800",
-                        on_click=NumeroVelocidad.calcTomato,
-                    ),
-                    rx.button(
-                        "Limpiar",
-                        class_name="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800",
-                        on_click=NumeroVelocidad.limpiar,
-                    ),
-                ),
-
-                rx.hstack(
-                    rx.cond(
-                        NumeroVelocidad.numVisible,
-                        rx.text(
-                            "Total de PM es: ", NumeroVelocidad.totalTomato, " palabras por minuto",
-                            size="5",
-                        ),
-                        rx.divider(),
-                    ),
-                ),
-
-                spacing="2",
-                align="center",
-            ),
-        ),
-        # class_name="bg-blue-500",
-        bg="var(--primary-color)",
+        class_name="flex items-center justify-center flex-wrap bg-teal-500 p-6",
         width="100%",
     ),
 
